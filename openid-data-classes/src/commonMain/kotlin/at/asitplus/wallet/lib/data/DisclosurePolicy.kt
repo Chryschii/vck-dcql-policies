4d30a477dbe46a21e0f110a7b993addb418291fc
Christian Wolfgang Burtscher <christian.burtscher@student.tugraz.at>
2026-02-03 10:14:09 +0100

    Defined new dataclass for relyingpartyattributes

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/DisclosurePolicy.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/DisclosurePolicy.kt
@@ -1,4 +1,5 @@
 package at.asitplus.wallet.lib.data
+
 import at.asitplus.openid.dcql.DCQLQuery
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -6,16 +7,13 @@ import kotlinx.serialization.Serializable
 /**
  * Represents an embedded disclosure policy for a credential.
  *
- * This policy defines which claims a specific relying party is authorized to request.
- *
- * @property relyingPartyId A unique identifier for the Relying Party (Verifier) this policy applies to.
- * @property policy A DCQL query that specifies the set of credentials and claims that are
- *                  permissible to be disclosed to this relying party.
+ * @property relyingPartyAttributes Attributes identifying which relying parties this policy applies to.
+ * @property policy A DCQL query specifying which credentials and claims may be disclosed.
  */
 @Serializable
 data class DisclosurePolicy(
-    @SerialName("relyingPartyId")
-    val relyingPartyId: String,
+    @SerialName("relyingPartyAttributes")
+    val relyingPartyAttributes: RelyingPartyAttributes,
     @SerialName("policy")
     val policy: DCQLQuery,
 )
